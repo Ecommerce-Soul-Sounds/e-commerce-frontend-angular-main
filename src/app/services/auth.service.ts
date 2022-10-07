6ed92b6114c97d8806028cb6bba588c78ca4fc7a
vklane <varak97@gmail.com>
2022-10-07 12:30:23 -0400

    changes to auth srvs

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,6 +9,8 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   authUrl: string = `${environment.baseUrl}/auth`;
+  userUrl: string = `${environment.baseUrl}/user`; 
+
   loggedIn: boolean = false;
 
   constructor(private http: HttpClient) { }
@@ -19,11 +21,20 @@ export class AuthService {
   }
 
   logout(): void{
-    this.http.post(`${this.authUrl}/logout`, null);
+    this.http.post(`${this.authUrl}/logout`, null, {headers: environment.headers, withCredentials: environment.withCredentials}).subscribe(
+      () => {
+        console.log("LOGGED OUT");
+      }
+    );
   }
 
-  register(firstName: string, lastName: string, email: string, password: string): Observable<any> {
-    const payload = {firstName: firstName, lastName: lastName, email: email, password: password};
+  register(firstName: string, lastName: string, email: string, password: string, street_address: string, apt: string, city:string, state:string, zipcode:number): Observable<any> {
+    const payload = {firstName: firstName, lastName: lastName, email: email, password: password, street_address: street_address, apt:apt, city:city, state:state, zipcode:zipcode};
     return this.http.post<any>(`${this.authUrl}/register`, payload, {headers: environment.headers});
   }
+  userForm(firstname: string, lastname: string, email: string, password: string, streetaddress: string, city:string, state:string, zipcode:number,profile:string): Observable<any> {
+    const payload = {firstname: firstname, lastname: lastname, email: email, password: password, streetaddress: streetaddress, city:city, state:state, zipcode:zipcode,profile:profile};
+    return this.http.put<any>(`${this.userUrl}/update`, payload, {headers: environment.headers});
+  }
 }
+
